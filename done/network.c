32d5c3f0298c785de61bff9a8f6a7722a48a8cf8
antoine-crettenand <antoine.crettenand@gmail.com>
2018-03-27 19:12:20 +0200

    Nrealy finished week05

--- done/network.c
+++ done/network.c
@@ -10,16 +10,13 @@
 
 
 error_code network_get(client_t client, pps_key_t key, pps_value_t *value){
-	
 	// Set up socket with a receive timeout of 1 s.
     int s = client.socket;
     
-
     // Load server address.
     node_t srv_addr = client.server;
     get_server_addr(PPS_DEFAULT_IP, PPS_DEFAULT_PORT, &srv_addr);
 
-    // Read user input from stdin.
     pps_key_t request = key;
 
     // Prepare outgoing message with htonl.
@@ -34,20 +31,32 @@ error_code network_get(client_t client, pps_key_t key, pps_value_t *value){
       return ERR_NETWORK;
 
     // Receive response.
-    unsigned int in_msg;
+    unsigned pps_value_t in_msg;
     ssize_t in_msg_len = recv(s, &in_msg, sizeof(in_msg), 0);
     if (in_msg_len == sizeof(in_msg)) { 
 		// Valid response.
-        // Parse response with ntohl
+        // Parse response with ntohl.
         pps_value_t response;
         response = ntohl(in_msg);
 		*value = response;
 	}	
-
     return ERR_NONE;
 }
 
 error_code network_put(client_t client, pps_key_t key, pps_value_t value){
 	
+	// Set up socket with a receive timeout of 1 s.
+    int s = client.socket;
+    
+    // Load server address.
+    node_t srv_addr = client.server;
+    get_server_addr(PPS_DEFAULT_IP, PPS_DEFAULT_PORT, &srv_addr);
+    
+    pps_key_t request = key;
+
+    // Prepare outgoing message with htonl.
+    unsigned int out_msg;
+    out_msg = htonl(request);
+    
 	return ERR_NONE;
 }
--- done/pps-launch-server.c
+++ done/pps-launch-server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 
 #include "system.h"
+#include "network.h"
 #include "config.h"
 #include "error.h"
 
@@ -21,27 +22,43 @@ int main(void){
 	
 	//Receive messages forever
 	while(1){
-		struct sockaddr_in cli_addr;
+		node_t cli_addr;
 		socklen_t addr_len = sizeof(cli_addr);
 		memset(&cli_addr, 0, addr_len);
 		
-		unsigned char* in_msg;
+		pps_key_t in_msg;
         ssize_t in_msg_len = recvfrom(s, &in_msg, sizeof(in_msg), 0,
                                       (struct sockaddr *) &cli_addr, &addr_len);
         
         if (in_msg_len != sizeof(in_msg)) { // Wrong message size.
             printf("Received invalid message");
             continue;
-        }     
+        }
       
 		// Write Request
 		if (in_msg_len == 5){
-			
+			sendto(s, 0, 0, 0,
+               (struct sockaddr *) &cli_addr, sizeof(cli_addr));
 		}
 		
 		// Read Request
 		if (in_msg_len == 1){
 			
+		pps_key_t request;
+		request = ntohl(in_msg);	
+			
+		client_t client;
+		client.server = cli_addr;
+		client.socket = s;
+			
+		pps_value_t get;
+		network_get(client, request, &get);
+			
+		unsigned int out_msg;
+		out_msg = htonl(get);
+			
+		sendto(s, &out_msg, sizeof(out_msg), 0,
+              (struct sockaddr *) &cli_addr, sizeof(cli_addr));
 		}
 	}
 	
