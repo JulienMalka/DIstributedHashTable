823e9ec8c94b301172dc19b80f31deb7d49ed1d5
antoine-crettenand <antoine.crettenand@gmail.com>
2018-03-28 17:24:08 +0200

    Need for initializin client

--- done/pps-client-put.c
+++ done/pps-client-put.c
@@ -17,11 +17,23 @@ int main(void){
 	//Bind server to the address:port
 	bind_server(s, PPS_DEFAULT_IP, PPS_DEFAULT_PORT);
 	
+	client_t client;
+	
+	
 	while(1){
 		
+		pps_value_t value;
+		pps_key_t key;
+		int ok = 1;
+				
+		while (ok == 1){		
+			int error = scanf("%c %d", &key, &value);
+			if (error != 1) ok = 0;
+			else printf("FAIL\n");
+		}
+		printf("OK\n");
 		
-		
-		
+		network_put(client, key, value);
 	}
 	
 	
