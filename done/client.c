3d8ca7a16c94f1da02ce4f9922f8bd073d1ef563
antoine-crettenand <antoine.crettenand@gmail.com>
2018-04-11 10:45:55 +0200

    nothing

--- done/client.c
+++ done/client.c
@@ -5,19 +5,18 @@
  * @brief does all the work to be done at the end of life of a client
  * @param client the client to end
  */
-void client_end(client_t *client){
-  node_end();
+void client_end(client_t *client)
+{
+    node_end();
 }
 
-error_code client_init(client_init_args_t){
-
-client_init_args_t.client->name = client_init_args_t.name;
-node_t node;
-const char ip = PPS_DEFAULT_IP;
-node_init(&node, &ip, PPS_DEFAULT_PORT, 0);
-client_init_args_t.client->server = node;
-client_init_args_t.client->socket = get_socket(1);
-return 0;
-
-
+error_code client_init(client_init_args_t)
+{
+    client_init_args_t.client->name = client_init_args_t.name;
+    node_t node;
+    const char ip = PPS_DEFAULT_IP;
+    node_init(&node, &ip, PPS_DEFAULT_PORT, 0);
+    client_init_args_t.client->server = node;
+    client_init_args_t.client->socket = get_socket(1);
+    return 0;
 }
