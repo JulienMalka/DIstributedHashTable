a0ff0f982636f54f087b8f4636844c9bc6b0fe59
JulienMalka <julien.malka@me.com>
2018-04-11 10:43:42 +0200

    fixed compilation errors

--- done/client.c
+++ done/client.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include "client.h"
-
+#include "config.h"
+#include "system.h"
 /**
  * @brief does all the work to be done at the end of life of a client
  * @param client the client to end
  */
 void client_end(client_t *client){
-  node_end();
+  node_end(&(client->server));
 }
 
-error_code client_init(client_init_args_t){
+error_code client_init(client_init_args_t client_init_args){
 
-client_init_args_t.client->name = client_init_args_t.name;
+client_init_args.client->name = client_init_args.name;
 node_t node;
 const char ip = PPS_DEFAULT_IP;
 node_init(&node, &ip, PPS_DEFAULT_PORT, 0);
-client_init_args_t.client->server = node;
-client_init_args_t.client->socket = get_socket(1);
+client_init_args.client->server = node;
+client_init_args.client->socket = get_socket(1);
 return 0;
 
 
