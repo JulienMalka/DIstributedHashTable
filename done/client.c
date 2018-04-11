1e650fe6ac396f6f0609f48e527db2e8e0813c22
JulienMalka <julien.malka@me.com>
2018-04-11 10:30:41 +0200

    client init

--- done/client.c
+++ done/client.c
@@ -8,3 +8,16 @@
 void client_end(client_t *client){
   node_end();
 }
+
+error_code client_init(client_init_args_t){
+
+client_init_args_t.client->name = client_init_args_t.name;
+node_t node;
+const char ip = PPS_DEFAULT_IP;
+node_init(&node, &ip, PPS_DEFAULT_PORT, 0);
+client_init_args_t.client->server = node;
+client_init_args_t.client->socket = get_socket(1);
+return 0;
+
+
+}
