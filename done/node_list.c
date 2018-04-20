a07f37ef7d15384d7e31c96277cd55868589c90f
antoine-crettenand <antoine.crettenand@gmail.com>
2018-04-20 14:54:30 +0200

    parsing debugged

--- done/node_list.c
+++ done/node_list.c
@@ -6,18 +6,18 @@
 
 node_list_t *node_list_new()
 {
-	node_list_t* new = malloc(sizeof(node_list_t));
-	new->size = 0;
-	new->allocated_size = 32;
-	new->nodes = calloc(32, sizeof(node_t));
-	return new;
+    node_list_t* new = malloc(sizeof(node_list_t));
+    new->size = 0;
+    new->allocated_size = 32;
+    new->nodes = calloc(32, sizeof(node_t));
+    return new;
 }
 
 
 node_list_t *get_nodes()
 {
 
-	node_list_t* nodes = node_list_new();
+    node_list_t* nodes = node_list_new();
 
     FILE* file = fopen(PPS_SERVERS_LIST_FILENAME, "r");
 
@@ -25,75 +25,75 @@ node_list_t *get_nodes()
         //gestion de l'erreur
     }
 
-	int current_char = fgetc(file);
+    int current_char = fgetc(file);
 
-	char ip[15];
-	int index = 0;
-	int port = 1;
+    char ip[15];
+    int index = 0;
+    uint16_t port = 0;
 
-        while(!feof(file)) {
-			printf("current char = %c\n", current_char);
+    while(!feof(file)) {
+        printf("current char = %c\n", current_char);
 
-			if (isspace(current_char)){
-				printf("Found space and index = %d\n", index);
-				current_char = fgetc(file);
-				fscanf(file, "%d", &port);
-				
-				//take the n first chars, discard else
-				char real_ip[index];
-				
-				for (int i = 0; i < index; i++){
-					real_ip[i] = ip[i];
-				}
-				
-	//			real_ip = {'1','2','7','.','0','.','0','.','1'};
+        if (isspace(current_char)) {
+            printf("Found space and index = %d\n", index);
+            fscanf(file, "%hu", &port);
+            current_char = fgetc(file);
 
-				node_t node;
-				printf("current ip = %s\n", real_ip);
-				node_init(&node, real_ip, port, 0);
-				node_list_add(nodes, node);
+            //take the n first chars, discard else
+            char* real_ip = calloc(index, sizeof(char));
 
-				while(current_char != '\n') {
-					current_char = fgetc(file);
-				}
-				index = 0;
-	//			current_char = fgetc(file);
-			}
+            for (int i = 0; i < index; i++) {
+                real_ip[i] = ip[i];
+            }
 
-			ip[index] = current_char;
+            node_t node;
+            printf("current ip = %s and port = %hu\n", real_ip, port);
+            node_init(&node, real_ip, port, port);
+            node_list_add(nodes, node);
 
-			index += 1;
-			current_char = fgetc(file);
+            while(current_char != '\n') {
+                current_char = fgetc(file);
+            }
+            index = 0;
+            //			current_char = fgetc(file);
         }
 
-	return nodes;
+        ip[index] = current_char;
+
+        index += 1;
+        current_char = fgetc(file);
+    }
+
+    return nodes;
 }
 
-error_code node_list_add(node_list_t *list, node_t node){
+error_code node_list_add(node_list_t *list, node_t node)
+{
 
-	list->size++;
-	if(list->size > list->allocated_size){
+    list->size++;
+    if(list->size > list->allocated_size) {
 
-		node_t* nodes = realloc(list->nodes, list->allocated_size+32);
-		if(nodes==NULL){
-			return ERR_NOMEM;
-			}else{
-		list->allocated_size+=32;
-	}
-	}
+        node_t* nodes = realloc(list->nodes, list->allocated_size+32);
+        if(nodes==NULL) {
+            return ERR_NOMEM;
+        } else {
+            list->allocated_size+=32;
+        }
+    }
 
-	list->nodes[list->size-1] = node;
-	return ERR_NONE;
+    list->nodes[list->size-1] = node;
+    return ERR_NONE;
 
 }
 
 
 
-void node_list_free(node_list_t *list){
+void node_list_free(node_list_t *list)
+{
 
-for(int i=0; i<list->size;i++){
-	node_end(&list->nodes[i]);
-	}
+    for(int i=0; i<list->size; i++) {
+        node_end(&list->nodes[i]);
+    }
 
-	free(list);
+    free(list);
 }
--- done/tests.c
+++ done/tests.c
@@ -20,55 +20,57 @@
 START_TEST(week04)
 {
     Htable_t table;
-	
-	// Defining tests ressource
-	const pps_key_t key1 = 'c';
-	const pps_value_t value1 = 42;
-	const pps_key_t key2 = 'a';
-	const pps_value_t value2 = 0;
-	
-	add_Htable_value(table, key1, value1);
-	add_Htable_value(table, key2, value2);
-	
-	const pps_value_t value_read1 = get_Htable_value(table, key1);
-	const pps_value_t value_read2 = get_Htable_value(table, key2);
 
-	// tests for input value = read value from get method
-	ck_assert_int_eq(value1, value_read1);
-	ck_assert_int_eq(value2, value_read2);
-	
-	const size_t hashed = hash_function(key1, 0);
-	ck_assert_int_eq(0, hashed);
-	
-	const size_t maxSize = SIZE_MAX;
-	ck_assert_int_eq(0, hash_function(key1, maxSize)); 
-	
-	// tests if reference isn't null
-	ck_assert_ptr_nonnull(table);
-	
-	// tests if method outputs correct errors on inputs
-	ck_assert_bad_param(add_Htable_value(NULL, key1, value1));
-	ck_assert_err_none(add_Htable_value(table, key1, value1));
-	
+    // Defining tests ressource
+    const pps_key_t key1 = 'c';
+    const pps_value_t value1 = 42;
+    const pps_key_t key2 = 'a';
+    const pps_value_t value2 = 0;
+
+    add_Htable_value(table, key1, value1);
+    add_Htable_value(table, key2, value2);
+
+    const pps_value_t value_read1 = get_Htable_value(table, key1);
+    const pps_value_t value_read2 = get_Htable_value(table, key2);
+
+    // tests for input value = read value from get method
+    ck_assert_int_eq(value1, value_read1);
+    ck_assert_int_eq(value2, value_read2);
+
+    const size_t hashed = hash_function(key1, 0);
+    ck_assert_int_eq(0, hashed);
+
+    const size_t maxSize = SIZE_MAX;
+    ck_assert_int_eq(0, hash_function(key1, maxSize));
+
+    // tests if reference isn't null
+    ck_assert_ptr_nonnull(table);
+
+    // tests if method outputs correct errors on inputs
+    ck_assert_bad_param(add_Htable_value(NULL, key1, value1));
+    ck_assert_err_none(add_Htable_value(table, key1, value1));
+
 }
 END_TEST
 
-void print_nodes(node_t* nodes, size_t size){
-	
-	char buffer[20];
-	
-	for (int i = 0; i < size; i++){
-		printf("node %d address = %s port = %hu\n", i, inet_ntop(AF_INET, &nodes[i].sin_addr, buffer, 20), nodes[i].sin_port);	
-	}
-	
+void print_nodes(node_t* nodes, size_t size)
+{
+
+    char buffer[20];
+
+    for (int i = 0; i < size; i++) {
+        printf("node %d address = %s port = %hu\n", i, inet_ntop(AF_INET, &nodes[i].sin_addr, buffer, 20), ntohs(nodes[i].sin_port));
+    }
+
 }
 
-START_TEST(week05){
-	node_list_t* nodes = node_list_new();
-	
-	nodes = get_nodes();
-	
-	print_nodes(nodes->nodes, nodes->size);
+START_TEST(week05)
+{
+    node_list_t* nodes = node_list_new();
+
+    nodes = get_nodes();
+
+    print_nodes(nodes->nodes, nodes->size);
 }
 END_TEST
 
