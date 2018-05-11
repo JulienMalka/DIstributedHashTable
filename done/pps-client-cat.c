3e0b10c7f0c007959925c60411c65f6e3e9d17f4
antoine-crettenand <antoine.crettenand@gmail.com>
2018-05-11 14:49:45 +0200

    implemented

--- done/pps-client-cat.c
+++ done/pps-client-cat.c
@@ -4,6 +4,7 @@
 
 #include "network.h"
 #include "error.h"
+#include "util.h"
 
 int main(int argc, char* argv[])
 {
@@ -24,6 +25,12 @@ int main(int argc, char* argv[])
         printf("FAIL\n");
         return -1;
     }
+    
+    /* Not enough mandatory arguments */
+    if (argv_size(argv) < 2){
+		printf("FAIL\n");
+		return -1;
+	}
 
     /* Parse the key */
     char* keys[300];
@@ -37,32 +44,37 @@ int main(int argc, char* argv[])
     nbr_arg--;
 
     /* The last key is the output key */
-    char* output_key = keys[nbr_arg - 1];
+    char* output_key = keys[nbr_arg];
 
     pps_value_t value_from;
     error_code error_code = network_get(client, keys[0], &value_from);
 
     if (error_code != ERR_NONE) {
-        printf("it failed in the first get\n");
+//        printf("it failed in the first get\n");
         printf("FAIL\n");
         return -1;
     }
 
     char* concat = value_from;
+    
+//    printf("nbr_arg = %lu\n", nbr_arg);
 
     /* Get the respective values and concatenate them */
     for (size_t i = 1; i < nbr_arg; i++) {
 
         error_code = network_get(client, keys[i], &value_from);
 
+//		printf("i = %lu get the value %s and current concat = %s\n",i ,value_from, concat);
+
         if (error_code != ERR_NONE) {
-            printf("it failed in one of the get\n");
             printf("FAIL\n");
             return -1;
         }
 
-        snprintf(concat, strlen(concat) + strlen(value_from) + 1, "%s%s", concat, value_from);
+        strcat(concat, value_from);
     }
+    
+//    printf("put the string %s at the key value %s\n", concat, output_key); 
 
     error_code = network_put(client, output_key, concat);
 
