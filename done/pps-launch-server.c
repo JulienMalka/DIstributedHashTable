77a15925db9a6730f52a02f8f2ac46a1b06b4953
Julien Malka <julien.malka@epfl.ch>
2018-06-03 18:29:04 +0200

    made server more robust

--- done/pps-launch-server.c
+++ done/pps-launch-server.c
@@ -24,10 +24,10 @@ int main(void)
 
     Htable_t h_table = construct_Htable(HTABLE_SIZE);
 
-    int ok = 1;
+    int not_parsed = 1;
     char ip[16]; //max 15 characters in an ip adress
     int port;
-    while(ok) {
+    while(not_parsed) {
         printf("IP port? ");
         int count_rec = scanf(" %15s %d", ip, &port);
         if(strlen(ip)>15) {
@@ -36,7 +36,7 @@ int main(void)
         error_code error_bind =  bind_server(s, ip, port);
 
         if (count_rec == 2 && error_bind == ERR_NONE)
-            ok = 0;
+            not_parsed = 0;
         else {
             printf("FAIL\n");
         }
@@ -52,6 +52,10 @@ int main(void)
         socklen_t addr_len = sizeof(cli_addr);
         memset(&cli_addr, 0, addr_len);
         char* in_msg = malloc(MAX_MSG_SIZE);
+        if(in_msg == NULL){
+          printf("FAIL\n");
+          continue;
+        }
         ssize_t in_msg_len = recvfrom(s, in_msg, MAX_MSG_SIZE, 0, (struct sockaddr *) &cli_addr, &addr_len);
         if (in_msg_len > MAX_MSG_SIZE) { // Wrong message size.
             printf("FAIL\n");
@@ -72,7 +76,11 @@ int main(void)
                 size_t counter = 0;
                 size_t size_packet =0;
                 /* 65507 max packet size */
-                char* packet = calloc(65507, sizeof(char));
+                char* packet = calloc(MAX_MSG_SIZE, sizeof(char));
+                if(packet == NULL){
+                  printf("FAIL\n");
+                  continue;
+                }
                 char header[4];
                 header[0] = node_dump->size >> 24;
                 header[1] = node_dump->size >> 16;
@@ -82,7 +90,7 @@ int main(void)
                 while(counter < node_dump->size) {
 
                     size_t size_kv = strlen(node_dump->list[counter].key) +1 + strlen(node_dump->list[counter].value);
-                    if(size_packet + size_kv < 65507) {
+                    if(size_packet + size_kv < MAX_MSG_SIZE) {
                         char* kv_request;
                         if(counter == 0) {
                             char* key_new = calloc(4+strlen(node_dump->list[counter].key), sizeof(char));
@@ -129,6 +137,10 @@ int main(void)
                 size_t size_value = in_msg_len - strlen(in_msg);
                 char* key = calloc(strlen(in_msg)+1, sizeof(char));
                 char* value = calloc(size_value, sizeof(char));
+                if(key == NULL || value == NULL){
+                  printf("FAIL\n");
+                  continue;
+                }
                 parse_put_request(in_msg, in_msg_len, key, value);
                 if (add_Htable_value(h_table, key, value) == ERR_NONE)
                     sendto(s, NULL, 0, 0,
@@ -140,6 +152,10 @@ int main(void)
         else if (memchr(in_msg, 0, in_msg_len)==NULL) {
             char* request = in_msg;
             pps_value_t get = malloc(MAX_MSG_ELEM_SIZE);
+            if(get == NULL){
+              printf("FAIL\n");
+              continue;
+            }
             get = get_Htable_value(h_table, request);
             if(get != NULL) {
 
